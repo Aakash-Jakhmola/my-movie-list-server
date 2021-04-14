8cab7432200a2b2b7030e2f91bb6e15a171073e6
sumit <sumitks866@gmail.com>
2021-04-14 20:56:33 +0530

    add /users/register,users/:userid and users/:userid/movielist route

--- models/user.js
+++ models/user.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose') ;
 
+var ValidateEmail = function(email) {
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return re.test(email)
+};
+
+
 const userSchema = mongoose.Schema({
-    name : String ,
-    email : String, 
+    firstname : String ,
+    lastname : String,
+    email: {
+		type: String,
+		trim: true,
+		lowercase: true,
+		unique: true,
+    	required: 'Email address is required',
+		// we can use either validate or match 
+    	validate: [ValidateEmail, 'Please fill a valid email address'],
+		//match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+	},
     moviesList : [{
         movieId : Number, 
         rating : Number
     }],
     friendsList : [{
-        userId : String,
+        userId : mongoose.Schema.Types.ObjectId
     }],
 }) ;
 
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,49 @@ const router = express.Router()
 
 const {User} = require('../models/user.js')
 
-router.post('/', function(req, res){
-    res.render("home", {data : []}) ;
+router.post('/register', function(req, res){
+    res.contentType('application/json')
     const user1 = User({
-        name : "aakash",
-        email : "aakash001jakhmola@gmail.com",
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+        email : req.body.email,
 
     });
-    user1.save().then(()=> {console.log('Successful');}) ;
+    user1.save()
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(err=>{
+            res.json(err)
+        })
 }) ;
+
+router.get('/:userid', function(req,res){
+    res.contentType('application/json')
+    const id = req.params.userid
+
+    User.findById(id)
+    .then(result =>{
+        res.json(result)
+    })
+    .catch(err => {
+        res.send(err)
+    })
+
+})
+
+router.get('/:userid/movielist', function(req,res){
+    res.contentType('application/json')
+    const id = req.params.id
+
+    User.find(id)
+    .then(result =>{
+        res.json(result.moviesList)
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
 
 module.exports = router

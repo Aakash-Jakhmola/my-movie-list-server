4971ba184a6191de143b6c7693d4a3bd4070d67e
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2022-02-16 22:28:24 +0530

    add getAccount domain

--- src/domain/user/getAccount.js
+++ src/domain/user/getAccount.js
@@ -1,7 +1,7 @@
 const { User } = require("../../database/models");
 const ErrorHandler = require("../../utils/errorHandler");
 
-async function getAccount({username, search}) {
+async function getAccount({ username, search, viewer }) {
   if(username) {
     const foundUser = await User.findOne({username});
     if(!foundUser) {
@@ -10,7 +10,43 @@ async function getAccount({username, search}) {
         message: `could not find user with username: ${username}`,
       })
     }
-  }
+    return foundUser;
+  } 
+
+  const users = await User.find({
+    $or: [
+      { username: { $regex: search, $options: "i" } },
+      { firstname: { $regex: search, $options: "i" } },
+      { lastname: { $regex: search, $options: "i" } }
+    ]
+  });
+  let result = [];
+  let usernameToFollowMap = {};
+  let usernames = users.map((u) => u.username);
+
+  const followingData = await Follow.aggregate([
+    {
+      $match: {
+        $and: [
+          { follower: viewer },
+          { following: { $in: usernames } }
+        ]
+      }
+    },
+  ]);
+  followingData.map((r) => {
+    usernameToFollowMap[r.following] = true;
+  })
+
+  docs.map((user) => {
+    result.push({
+      user_details: user,
+      is_following: usernameToFollowMap[user.username] != null
+    });
+  })
+  
+  return result;
+  
 }
 
 module.exports = getAccount;
